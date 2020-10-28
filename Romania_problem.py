ad3566be632c22c8a15ab7a8218bfddaa9c38ee1
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2020-10-28 11:35:21 +0530

    Romania Map Dict
    Just dictionary part.

---
+++ Romania_problem.py
@@ -0,0 +1,15 @@
+#Romania Problem with dictionary
+distance_problem=Dict(
+    Arad=Dict(Zerind=75, Sibiu=140, Timisoara=118),
+    Bucharest=Dict(Urziceni=85, Pitesti=101, Giurgiu=90, Fagaras=211),
+    Craiova=Dict(Drobeta=120, Rimnicu=146, Pitesti=138),
+    Drobeta=Dict(Mehadia=75),
+    Etorie=Dict(Hirsova=86),
+    Fagaras=Dict(Sibiu=99),
+    Hirsova=Dict(Urziceni=98),
+    Iasi=Dict(Vaslui=92, Neamt=87),
+    Lugoj=Dict(Timisoara=111, Mehadia=70),
+    Oradea=Dict(Zerind=71, Sibiu=151),
+    Pitesti=Dict(Rimnica=97),
+    Rimnicu=Dict(Sibiu=80),
+    Uraziceni=Dict(Vaslui=142))
